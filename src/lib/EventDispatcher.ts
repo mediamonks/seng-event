9f7f8005b8ebb7e26feb3cff586df03d9a4c49cd
Floris Bernard <floris.bernard@gmail.com>
2016-07-07 18:41:06 +0200

    Fix removal of event listeners
    Make sure that event listeners that are removed are no longer
    executed, even for the current event loop.
    
    Make sure that removing event listeners does not break the
    current event loop by creating a local copy of the array
    with listeners during execution.
    
    Renamed 'type' parameters to 'eventType'. We don't use type
    because it is a reserved keyword in TS.

--- src/lib/EventDispatcher.ts
+++ src/lib/EventDispatcher.ts
@@ -21,7 +21,7 @@ export default class EventDispatcher extends Disposable implements IEventDispatc
 
 	public dispatchEvent(event:IEvent):boolean
 	{
-		if (this.isDisposed())
+		if(this.isDisposed())
 		{
 			// todo: _log.error("Can't dispatchEvent on a disposed EventDispatcher");
 		}
@@ -33,28 +33,28 @@ export default class EventDispatcher extends Disposable implements IEventDispatc
 			event.target = this._target;
 			event.eventPhase = callTree.length === 1 ? EventPhase.AT_TARGET : EventPhase.CAPTURING_PHASE;
 
-			for (let i = 0; i < callTree.length; i++)
+			for(let i = 0; i < callTree.length; i++)
 			{
 				const currentTarget = callTree[i];
 				event.currentTarget = currentTarget;
-				if (currentTarget === this)
+				if(currentTarget === this)
 				{
 					event.eventPhase = EventPhase.AT_TARGET;
 				}
 
 				const propagationIsStopped = callListeners(currentTarget._listeners, event);
-				if (propagationIsStopped)
+				if(propagationIsStopped)
 				{
 					event.eventPhase = EventPhase.NONE;
 					break;
 				}
 
-				if (i === callTree.length - 1)
+				if(i === callTree.length - 1)
 				{
 					// after last target in tree, reset eventPhase to NONE
 					event.eventPhase = EventPhase.NONE;
 				}
-				else if (currentTarget === this)
+				else if(currentTarget === this)
 				{
 					// after target === currentTarget we will enter the bubbling phase
 					event.eventPhase = EventPhase.BUBBLING_PHASE;
@@ -66,44 +66,44 @@ export default class EventDispatcher extends Disposable implements IEventDispatc
 		return false;
 	}
 
-	public addEventListener(type:string, listener:Listener, useCapture:boolean = false, priority:number = 0):EventListenerData
+	public addEventListener(eventType:string, listener:Listener, useCapture:boolean = false, priority:number = 0):EventListenerData
 	{
-		if (typeof(this._listeners[type]) === 'undefined')
+		if(typeof(this._listeners[eventType]) === 'undefined')
 		{
-			this._listeners[type] = [];
+			this._listeners[eventType] = [];
 		}
 
 		// todo: log in debug mode
 		const isDebugMode = false;
-		if (isDebugMode && this.hasEventListener(type, listener, useCapture))
+		if(isDebugMode && this.hasEventListener(eventType, listener, useCapture))
 		{
 			// log.warn(trying to add double listener)
 		}
 		// end todo
 
-		const data:EventListenerData = new EventListenerData(this, type, listener, useCapture, priority);
-		this._listeners[type].push(data);
-		this._listeners[type].sort(this._listenerSorter);
+		const data:EventListenerData = new EventListenerData(this, eventType, listener, useCapture, priority);
+		this._listeners[eventType].push(data);
+		this._listeners[eventType].sort(this._listenerSorter);
 
 		return data;
 	}
 
-	public hasEventListener(type:string, listener?:Listener, useCapture?:boolean):boolean
+	public hasEventListener(eventType:string, listener?:Listener, useCapture?:boolean):boolean
 	{
-		if (typeof listener === 'undefined')
+		if(typeof listener === 'undefined')
 		{
-			return this._listeners[type] && this._listeners[type].length > 0;
+			return this._listeners[eventType] && this._listeners[eventType].length > 0;
 		}
-		else if (!this._listeners[type])
+		else if(!this._listeners[eventType])
 		{
 			return false;
 		}
 		else
 		{
-			for (let i = 0; i < this._listeners[type].length; i++)
+			for(let i = 0; i < this._listeners[eventType].length; i++)
 			{
-				const listenerData:EventListenerData = this._listeners[type][i];
-				if (listenerData.listener === listener && (typeof useCapture === 'undefined' || useCapture === listenerData.useCapture))
+				const listenerData:EventListenerData = this._listeners[eventType][i];
+				if(listenerData.listener === listener && (typeof useCapture === 'undefined' || useCapture === listenerData.useCapture))
 				{
 					return true;
 				}
@@ -112,44 +112,19 @@ export default class EventDispatcher extends Disposable implements IEventDispatc
 		}
 	}
 
-	public willTrigger(type:string):boolean
+	public willTrigger(eventType:string):boolean
 	{
-		return this.hasEventListener(type) || (this.parent && this.parent.willTrigger(type));
+		return this.hasEventListener(eventType) || (this.parent && this.parent.willTrigger(eventType));
 	}
 
-	public removeEventListener(type:string, listener:Listener, useCapture:boolean = false):void
+	public removeEventListener(eventType:string, listener:Listener, useCapture:boolean = false):void
 	{
-		if ((type in this._listeners) && (this._listeners[type] instanceof Array))
-		{
-			for (let i = this._listeners[type].length; i; i--)
-			{
-				let listenerData:EventListenerData = this._listeners[type][i - 1];
-				if (listenerData.listener === listener && listenerData.useCapture === useCapture)
-				{
-					this._listeners[type].splice(i - 1, 1);
-				}
-			}
-		}
-		else
-		{
-			// todo: _log.warn('trying to remove event that has no listeners "' + type + '"');
-		}
+		removeListenersFrom(this._listeners, false, eventType, listener, useCapture);
 	}
 
-	public removeAllEventListeners(type?:string):void
+	public removeAllEventListeners(eventType?:string):void
 	{
-		if (type === void 0)
-		{
-			this._listeners = {};
-		}
-		else if ((type in this._listeners) && (this._listeners[type] instanceof Array))
-		{
-			this._listeners[type].length = 0;
-		}
-		else
-		{
-			// todo: _log.warn('trying to remove all events that does not exist "' + type + '"');
-		}
+		removeListenersFrom(this._listeners, true, eventType);
 	}
 
 	public dispose():void
@@ -165,17 +140,66 @@ export default class EventDispatcher extends Disposable implements IEventDispatc
 	}
 }
 
+export const removeListenersFrom = (listeners:EventListenerMap, removeAll:boolean,
+                                    eventType?:string, listener?:Listener, useCapture?:boolean) =>
+{
+	// build an array with arrays of events for each eventType we want to remove from
+	const removeFrom:Array<Array<EventListenerData>> = [];
+	if(eventType)
+	{
+		// eventType argument is set, just remove from this type
+		if((eventType in listeners) && (listeners[eventType] instanceof Array))
+		{
+			removeFrom.push(listeners[eventType]);
+		}
+	}
+	else
+	{
+		// eventType not set, add all event types with listeners
+		for(let i in listeners)
+		{
+			if(listeners.hasOwnProperty(i) && listeners[i] instanceof Array)
+			{
+				removeFrom.push(listeners[i]);
+			}
+		}
+	}
+
+	if(removeFrom.length)
+	{
+		for(let i = 0; i < removeFrom.length; i++)
+		{
+			const listenersForType:Array<EventListenerData> = removeFrom[i];
+
+			for(let j = listenersForType.length; j; j--)
+			{
+				let listenerData:EventListenerData = listenersForType[j - 1];
+				if(removeAll || (listenerData.listener === listener && listenerData.useCapture === useCapture))
+				{
+					listenersForType.splice(j - 1, 1);
+					// mark the listener as removed, because it might still be active in the current event loop
+					listenerData.isRemoved = true;
+				}
+			}
+		}
+	}
+	else
+	{
+		// todo: _log.warn('trying to remove event that has no listeners "' + type + '"');
+	}
+};
+
 export const getCallTree = (target:EventDispatcher, bubbles:boolean):Array<EventDispatcher> =>
 {
 	const callTree:Array<EventDispatcher> = [];
 	const parents:Array<EventDispatcher> = getParents(target);
 
-	for (let i = parents.length; i; i--)
+	for(let i = parents.length; i; i--)
 	{
 		callTree.push(parents[i - 1]);
 	}
 	callTree.push(target);
-	if (bubbles)
+	if(bubbles)
 	{
 		Array.prototype.push.apply(callTree, parents);
 	}
@@ -186,7 +210,7 @@ export const getParents = (target:EventDispatcher):Array<EventDispatcher> =>
 {
 	let currentTarget:EventDispatcher = target;
 	const parents:Array<EventDispatcher> = [];
-	while (currentTarget.parent)
+	while(currentTarget.parent)
 	{
 		currentTarget = currentTarget.parent;
 		parents.push(currentTarget);
@@ -196,20 +220,20 @@ export const getParents = (target:EventDispatcher):Array<EventDispatcher> =>
 
 export const callListeners = (listeners:EventListenerMap, event:IEvent):boolean =>
 {
-	const listenersOfType:Array<EventListenerData> = listeners[event.type] || [];
+	const listenersOfType:Array<EventListenerData> = listeners[event.type] ? [...listeners[event.type]] : [];
 	let propagationIsStopped = false;
 
-	for (let i = 0; i < listenersOfType.length; i++)
+	for(let i = 0; i < listenersOfType.length; i++)
 	{
 		const disabledOnPhase = listenersOfType[i].useCapture ? EventPhase.BUBBLING_PHASE : EventPhase.CAPTURING_PHASE;
-		if (event.eventPhase !== disabledOnPhase)
+		if(event.eventPhase !== disabledOnPhase && !listenersOfType[i].isRemoved)
 		{
 			const callResult:number = event.callListener(listenersOfType[i].listener);
 
-			if (callResult > CallListenerResult.NONE)
+			if(callResult > CallListenerResult.NONE)
 			{
 				propagationIsStopped = true;
-				if (callResult === CallListenerResult.IMMEDIATE_PROPAGATION_STOPPED)
+				if(callResult === CallListenerResult.IMMEDIATE_PROPAGATION_STOPPED)
 				{
 					break;
 				}
