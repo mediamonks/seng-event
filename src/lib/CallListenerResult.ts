20728de1e22f78b7bd2a666f86001ed0b0b3ec5a
Floris Bernard <floris.bernard@gmail.com>
2016-07-09 00:04:43 +0200

    Add more typedoc
    Docs for AbstractEvent, CallListenerResult and EventPhase

--- src/lib/CallListenerResult.ts
+++ src/lib/CallListenerResult.ts
@@ -1,6 +1,20 @@
+/**
+ * Enum that is returned by the [[AbstractEvent.callListener]] method
+ */
 const enum CallListenerResult {
+	/**
+	 * Indicates that neither [[IEvent.stopPropagation|stopPropagation]] nor
+	 * [[IEvent.stopImmediatePropagation|stopImmediatePropagation]] has been called
+	 */
 	NONE = 0,
+	/**
+	 * Indicates that [[IEvent.stopPropagation|stopPropagation]] has been called, but
+	 * [[IEvent.stopImmediatePropagation|stopImmediatePropagation]] hasn't
+	 */
 	PROPAGATION_STOPPED = 1,
+	/**
+	 * Indicates that [[IEvent.stopImmediatePropagation|stopImmediatePropagation]] has been called
+	 */
 	IMMEDIATE_PROPAGATION_STOPPED = 2
 }
 
