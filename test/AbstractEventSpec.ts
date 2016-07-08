3335d6fe75e9a3e79f046fcf407510c09723941e
Floris Bernard <floris@mediamonks.com>
2016-07-08 19:30:23 +0200

    Add unit tests for AbstractEvent

--- test/AbstractEventSpec.ts
+++ test/AbstractEventSpec.ts
@@ -1,13 +1,125 @@
 import AbstractEvent from '../src/lib/AbstractEvent';
 import {expect} from 'chai';
+import IEvent from "../src/lib/IEvent";
+import CallListenerResult from "../src/lib/CallListenerResult";
 
 describe('AbstractEvent', () =>
 {
-	describe('subclass', () =>
+	describe('#callListener', () =>
 	{
-		it('should be ok', () =>
+		describe('with a listener that calls event.stopPropagation()', () =>
 		{
-			expect('test').not.to.equal('foo');
+			class TestEvent extends AbstractEvent {
+				public clone():TestEvent
+				{
+					return new TestEvent(this.type, this.bubbles, this.cancelable);
+				}
+			}
+			const event = new TestEvent('test');
+			const handler = (event:IEvent) =>
+			{
+				event.stopPropagation();
+			};
+			const result = event.callListener(handler);
+
+			it('should return CallListenerResult.PROPAGATION_STOPPED', () =>
+			{
+				expect(result).to.equal(CallListenerResult.PROPAGATION_STOPPED);
+			});
+		});
+		describe('with a listener that calls event.stopImmediatePropagation()', () =>
+		{
+			class TestEvent extends AbstractEvent {
+				public clone():TestEvent
+				{
+					return new TestEvent(this.type, this.bubbles, this.cancelable);
+				}
+			}
+			const event = new TestEvent('test');
+			const handler = (event:IEvent) =>
+			{
+				event.stopImmediatePropagation();
+			};
+			const result = event.callListener(handler);
+
+			it('should return CallListenerResult.IMMEDIATE_PROPAGATION_STOPPED', () =>
+			{
+				expect(result).to.equal(CallListenerResult.IMMEDIATE_PROPAGATION_STOPPED);
+			});
+		});
+		describe('with a listener that calls event.stopPropagation() and event.stopImmediatePropagation()', () =>
+		{
+			class TestEvent extends AbstractEvent {
+				public clone():TestEvent
+				{
+					return new TestEvent(this.type, this.bubbles, this.cancelable);
+				}
+			}
+			const event = new TestEvent('test');
+			const handler = (event:IEvent) =>
+			{
+				event.stopImmediatePropagation();
+				event.stopPropagation();
+			};
+			const result = event.callListener(handler);
+
+			it('should return CallListenerResult.IMMEDIATE_PROPAGATION_STOPPED', () =>
+			{
+				expect(result).to.equal(CallListenerResult.IMMEDIATE_PROPAGATION_STOPPED);
+			});
+		});
+		describe('with a listener that doesn\'t call event.stopPropagation() or event.stopImmediatePropagation()', () =>
+		{
+			class TestEvent extends AbstractEvent {
+				public clone():TestEvent
+				{
+					return new TestEvent(this.type, this.bubbles, this.cancelable);
+				}
+			}
+			const event = new TestEvent('test');
+			const handler = () => {};
+			const result = event.callListener(handler);
+
+			it('should return CallListenerResult.NONE', () =>
+			{
+				expect(result).to.equal(CallListenerResult.NONE);
+			});
 		});
 	});
+
+	describe('#preventDefault()', () =>
+	{
+		describe('on a cancellable event', () =>
+		{
+			class TestEvent extends AbstractEvent {
+				public clone():TestEvent
+				{
+					return new TestEvent(this.type, this.bubbles, this.cancelable);
+				}
+			}
+			const event = new TestEvent('test', false, true);
+
+			it('should set defaultPrevented to true', () =>
+			{
+				event.preventDefault();
+				expect(event.defaultPrevented).to.be.true;
+			});
+		});
+		describe('on a non-cancellable event', () =>
+		{
+			class TestEvent extends AbstractEvent {
+				public clone():TestEvent
+				{
+					return new TestEvent(this.type, this.bubbles, this.cancelable);
+				}
+			}
+			const event = new TestEvent('test', false, false);
+
+			it('should throw an error', () =>
+			{
+				expect(() => event.preventDefault()).to.throw(Error);
+			});
+		});
+	});
+
 });
