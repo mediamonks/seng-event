f70fe8b9de285fb2ab0c0fa3ab480f23cd5693fa
Floris Bernard <floris.bernard@gmail.com>
2016-07-13 07:53:27 +0200

    Remove dist folder from repository
    Dist folder was added to .gitignore but not yet removed from
    repo

--- dist/es6/index.js
+++
@@ -1,9 +0,0 @@
-import { default as _export } from './lib/EventDispatcher';
-export { default as EventPhase } from './lib/EventPhase';
-export { default as EventListenerData } from './lib/EventListenerData';
-export { default as CallListenerResult } from './lib/CallListenerResult';
-export { default as AbstractEvent } from './lib/AbstractEvent';
-import './lib/event/CommonEvent';
-import './lib/event/BasicEvent';
-import './lib/util/eventTypeUtils';
-export default _export;
--- dist/es6/lib/event/BasicEvent.js
+++
@@ -1,7 +0,0 @@
-import AbstractEvent from "../AbstractEvent";
-class BasicEvent extends AbstractEvent {
-    clone() {
-        return new BasicEvent(this.type, this.bubbles, this.cancelable);
-    }
-}
-export default BasicEvent;
