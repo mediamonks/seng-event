1e7f4c26875cf6957085b44b14790b9c6318a9cc
Lars van Braam <lars@mediamonks.com>
2017-10-12 14:54:34 +0200

    Remove self assigned values of the eventPhase enum

--- src/lib/EventPhase.ts
+++ src/lib/EventPhase.ts
@@ -5,21 +5,21 @@ const enum EventPhase {
 	/**
 	 * Indicates that the event is currently not being dispatched
 	 */
-	NONE = 0,
+	NONE,
 	/**
 	 * Indicates that the event is in the capturing phase, moving down from the top-most EventDispatcher
 	 * instance to the parent of the target EventDispatcher
 	 */
-	CAPTURING_PHASE = 1,
+	CAPTURING_PHASE,
 	/**
 	 * Indicates that we are currently calling the event listeners on the event target during dispatch.
 	 */
-	AT_TARGET = 2,
+	AT_TARGET,
 	/**
 	 * Indicates that we are currently moving back up from the parent of the target EventDispatcher to
 	 * the top-most EventDispatcher instance.
 	 */
-	BUBBLING_PHASE = 3,
+	BUBBLING_PHASE,
 }
 
 
